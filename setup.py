b8e4770795cab87c4aa13ef8587f2f21c93eb0cc
vinay9597 <vinay80965@gmail.com>
2026-03-02 22:28:23 -0500

    setup updated1

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
- 
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgen",
+    version="0.1.0",
+    author="Vinay Yachamaneni",
+    author_email="vinay80965@gmail.com",
+    install_requires=[
+        "openai",
+        "langchain",
+        "streamlit",
+        "python-dotenv",
+        "pyPDF2",
+    ],
+    packages=find_packages(),
+)
